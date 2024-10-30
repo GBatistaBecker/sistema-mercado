8788475df220b02eccc7f5ba20f0e8985d966f4e
GBatistaBecker <guilhermebatb@gmail.com>
2024-10-30 14:48:10 -0300

    Initial commit

--- src/main/java/com/example/sistemamercado/HelloController.java
+++ src/main/java/com/example/sistemamercado/HelloController.java
@@ -1,14 +1,30 @@
 package com.example.sistemamercado;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+public class ProductController {
+    @FXML private TextField searchField;
+    @FXML private ComboBox<String> preferenceComboBox;
+    @FXML private TableView<Produtos> productTable;
+    @FXML private Label cartCountLabel;
+    @FXML private Button deliveryButton;
+
+    private ObservableList<Produtos> cart = FXCollections.observableArrayList();
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+        deliveryButton.setDisable(true); // Botão começa desativado
+    }
+
+    @FXML
+    private void updateCart(Produtos product) {
+        cart.add(produto);
+        cartCountLabel.setText("Itens no Carrinho: " + cart.size());
+        deliveryButton.setDisable(cart.size() < 20); // Ativa o botão com 20 itens ou mais
     }
 }
